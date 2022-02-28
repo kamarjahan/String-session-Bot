0f49410ac41f9f08c1a4460aff0b32cbbcab1f2d
KAMARJAHAN <92244714+kamarjahan@users.noreply.github.com>
2022-02-28 13:55:20 +0530

    Update Data.py

--- Data.py
+++ Data.py
@@ -4,15 +4,7 @@ from pyrogram.types import InlineKeyboardButton
 class Data:
     # Start Message
     START = """
-Hello {}
-
-If you don't trust this bot,
-1) don't read this message
-2) block bot or delete chat
-
-This Bot Works To Help You Get Session String Via Bot. Recommendations If You Want To Take String Use Another Account,
-Managed By @zenfrans
-    """
+Hello {} IAM A BOT USED FOR GENERATING YOUR TELETHON AND PYROGRAM STRING SESSION MAINTAINED BY [DEVOURDEVILS](t.me/devourdevils) """
 
     # Home Button
     home_buttons = [
@@ -27,19 +19,18 @@ Managed By @zenfrans
     # Rest Buttons
     buttons = [
         [InlineKeyboardButton("Start Generating Session", callback_data="generate")],
-        [InlineKeyboardButton("Maintaned By", url="https://t.me/zenfrans")],
+        [InlineKeyboardButton("Maintaned By", url="https://t.me/devourdevils")],
         [
             InlineKeyboardButton("How to use me", callback_data="help"),
             InlineKeyboardButton("About", callback_data="about")
       ],
-        [InlineKeyboardButton("Other bot info", url="https://t.me/nbzoning")],
+        [InlineKeyboardButton("Other bot info", url="https://t.me/devourdevils")],
     ]
 
 
     # Help Message
     HELP = """
 ✨ **Available Commands** ✨
-
 /about - About this bot
 /help - How to use this bot
 /start - Start Bot
@@ -51,14 +42,9 @@ Managed By @zenfrans
     # About Message
     ABOUT = """
 **About This Bot** 
-
-A telegram bot to retrieve pyrograms and telethon string sessions by @nbzoning
-
-Group Support : [Gabung](https://t.me/OkaeriUserbot)
-
+A telegram bot to retrieve pyrograms and telethon string sessions by @devourdevils
+Group Support : [Gabung](https://t.me/septemberfilms)
 Framework : [Pyrogram](docs.pyrogram.org)
-
 Language : [Python](www.python.org)
-
-Developer : @zenfrans
+Developer : @devourdevils
     """
